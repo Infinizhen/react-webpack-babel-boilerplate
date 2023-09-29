1e880cb62911f59cb0ae5a75dad5079df17359e5
Miguel Rodríguez <mrodriguezx@disgrafic.com>
2023-09-29 16:14:13 +0200

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,63 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	// Path to the output directory, where the bundle.js file will be created
-	output: {
-		path: path.join(__dirname, "/public"),
-		filename: "bundle.js",
-	},
-	devServer: {
-		port: 3000,
-	},
-	// Rules to compile and bundle the files.
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				loader: "babel-loader",
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	// Minimum plugins setup.
-	// HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles.
-	// MiniCssExtractPlugin extracts CSS into separate files. It creates a CSS file per JS file which contains CSS.
-	plugins: [
-		new HtmlWebpackPlugin({ template: "./src/index.html" }),
-		new MiniCssExtractPlugin(),
-	],
+  // Entry point of your application
+  entry: "./src/index.js",
+
+  // Output configuration for the bundle files
+  output: {
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js",
+    clean: true, // Cleans the output directory before each build
+  },
+
+  // Development server configuration
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, "public"), // Serves static files from 'public' directory
+    compress: true, // Enables gzip compression
+    hot: true, // Enables Hot Module Replacement
+    open: true, // Opens the browser after server has been started
+  },
+
+  // Module rules for processing different file types
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // Use MiniCssExtractPlugin loader instead of 'style-loader' for production
+      },
+    ],
+  },
+
+  // Plugins configuration
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css", // Use contenthash for cache busting
+    }),
+  ],
+
+  // Optimization and performance enhancements
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+
+  // Resolving module import statements
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Shortcut to reference src directory
+    },
+  },
+
+  // Source Map for easier debugging
+  devtool: "source-map",
 };
